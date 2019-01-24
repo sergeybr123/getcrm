9098d0df939b36e38774b1ced3d3e59b6cc4b46e
sergeybr123 <fox_ritek@mail.ru>
2019-01-24 18:17:05 +0600

    Update 2019-01-24

--- app/Http/Controllers/Admin/MultiLinkController.php
+++ app/Http/Controllers/Admin/MultiLinkController.php
@@ -18,34 +18,36 @@ class MultiLinkController extends Controller
         $whatsapp = Account::where('type', 'WhatsApp')->get();
 
         foreach ($whatsapp as $item) {
-            $company = Company::findOrFail($item->company_id);
+            $company = Company::where('id', $item->company_id)->whereNull('bot')->whereNull('temp_bot')->first();
 
-            $bot = new Bot();
-            $bot->type = "multilink";
-            $bot->botable_id = $item->company_id;
-            $bot->botable_type = "App\\Models\\Company";
-            $bot->name = $company->slug;
-            $bot->active = true;
-            $bot->save();
+            if($company != null) {
+                $bot = new Bot();
+                $bot->type = "multilink";
+                $bot->botable_id = $item->company_id;
+                $bot->botable_type = "App\\Models\\Company";
+                $bot->name = $company->slug;
+                $bot->active = true;
+                $bot->save();
 
-            $bot_listener = new BotListener();
-            $bot_listener->bot_id = $bot->id;
-            $bot_listener->text = "welcome";
-            $bot_listener->save();
+                $bot_listener = new BotListener();
+                $bot_listener->bot_id = $bot->id;
+                $bot_listener->text = "welcome";
+                $bot_listener->save();
 
-            $bot_answer = new BotAnswer();
-            $bot_answer->bot_listener_id = $bot_listener->id;
-            $bot_answer->data = ['text' => "Мы рады приветствовать вас!"];
-            $bot_answer->type = "GurmanAlexander\\TheBot\\Models\\Answers\\TextAnswer";
-            $bot_answer->order = 1;
-            $bot_answer->save();
+                $bot_answer = new BotAnswer();
+                $bot_answer->bot_listener_id = $bot_listener->id;
+                $bot_answer->data = ['text' => "Мы рады приветствовать вас!"];
+                $bot_answer->type = "GurmanAlexander\\TheBot\\Models\\Answers\\TextAnswer";
+                $bot_answer->order = 1;
+                $bot_answer->save();
 
-            $bot_answer = new BotAnswer();
-            $bot_answer->bot_listener_id = $bot_listener->id;
-            $bot_answer->data = ['text' => "Whatsapp", 'type' => 'whatsapp', 'phone' => str_replace(['+', ' ', '-', '(', ')', '_'], '', $item->account_service_id), 'wa_text' => 'Привет'];
-            $bot_answer->type = "GurmanAlexander\\TheBot\\Models\\Answers\\ActionAnswer";
-            $bot_answer->order = 2;
-            $bot_answer->save();
+                $bot_answer = new BotAnswer();
+                $bot_answer->bot_listener_id = $bot_listener->id;
+                $bot_answer->data = ['text' => "Whatsapp", 'type' => 'whatsapp', 'phone' => str_replace(['+', ' ', '-', '(', ')', '_'], '', $item->account_service_id), 'wa_text' => 'Привет'];
+                $bot_answer->type = "GurmanAlexander\\TheBot\\Models\\Answers\\ActionAnswer";
+                $bot_answer->order = 2;
+                $bot_answer->save();
+            }
         }
         try{
             return response()->json(['error' => 0, 'whatsapp' => count($whatsapp)]);
